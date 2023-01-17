b7b000f1bd4112434a9d1dd3f6af0e58b2a4ab1b
shehraj123 <shehrajsingh@gmail.com>
2023-01-16 19:10:27 -0700

    Made proxy server forked

--- proxy_server.py
+++ proxy_server.py
@@ -3,6 +3,7 @@ Proxy server for www.google.com
 """
 import socket
 from threading import Thread
+import os
 
 BYTES_TO_READ = 4096
 PROXY_SERVER_HOST = "127.0.0.1"
@@ -40,6 +41,7 @@ def handle_connection(conn, addr):
         response = send_request("www.google.com", 80, request)
         print(f"Got response from google: {response}")
         conn.sendall(response)
+        conn.shutdown(socket.SHUT_RDWR)
 
 def start_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -62,5 +64,22 @@ def start_threaded_server():
             thread = Thread(target=handle_connection, args=(conn, addr))
             thread.run()
 
+def start_forked_server():
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        server_socket.bind((PROXY_SERVER_HOST, PROXY_SERVER_PORT))
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_socket.listen(2)
+        
+        while True:
+            conn, addr = server_socket.accept()
+            
+            pid = os.fork()
+
+            if pid == 0:
+                handle_connection(conn, addr)
+                break
+            
+
+
 if __name__ == "__main__":
-    start_threaded_server()
+    start_forked_server()
